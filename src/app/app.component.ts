7d7cdbaac8ba25b0a2d5cb631ff58060d904e043
ekdev01 <31770879+ekdev01@users.noreply.github.com>
2018-10-12 20:15:46 +0200

    Update with comments

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,11 @@
+// importation package Component '@angular/core'
 import { Component } from '@angular/core';
 
+// definition du composant, qui doit contenir au moins 2 elements selector et template
 @Component({
   selector: 'my-app',
   template: `<h1>Hello {{name}}</h1>`,
 })
+
+// export du nouveau compsant ainsi cree pour etre disponible aux autres modules
 export class AppComponent  { name = 'Angular'; }
